d64d2253c82bd7b0904083af8c7614229c47ca5a
Germán Zittermann <germanzittermann@gmail.com>
2022-05-22 23:26:40 -0300

    Lab repository error fixed

--- src/main/java/com/phoneshop/repositories/LabRepository.java
+++ src/main/java/com/phoneshop/repositories/LabRepository.java
@@ -1,8 +1,9 @@
 package com.phoneshop.repositories;
 
+import com.phoneshop.entities.Lab;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LabRepository extends JpaRepository<LabRepository, Long> {
+public interface LabRepository extends JpaRepository<Lab, Long> {
 }
