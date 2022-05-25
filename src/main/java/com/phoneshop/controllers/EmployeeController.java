9b6f8634ab70e9b6ec3e1569177ec0b4932c7212
Germán Zittermann <germanzittermann@gmail.com>
2022-05-25 20:53:54 -0300

    Employee controller created

---
+++ src/main/java/com/phoneshop/controllers/EmployeeController.java
@@ -0,0 +1,100 @@
+package com.phoneshop.controllers;
+
+import com.phoneshop.entities.Employee;
+import com.phoneshop.repositories.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class EmployeeController {
+
+    private final Logger log = (Logger) LoggerFactory.getLogger(LabController.class);
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @GetMapping(value = {"/api/employees", "/api/employees/"})
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping(value = {"/api/employees/{id}", "/api/employees/{id}/"})
+    public ResponseEntity<Employee> findById(@PathVariable Long id) {
+
+        Optional<Employee> employeeOpt = employeeRepository.findById(id);
+
+        return  employeeOpt.isPresent() ?
+                ResponseEntity.ok(employeeOpt.get())
+                : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping(value = {"/api/employees/{employee}", "/api/employees/{employee}/"})
+    public ResponseEntity<Employee> create(@RequestBody Employee employee){
+
+        if(employee.getUserId() != null) {
+
+            log.warn("Trying to create am employee with id");
+            return ResponseEntity.badRequest().build();
+
+        }
+
+        Employee result = employeeRepository.save(employee);
+        return ResponseEntity.ok(result);
+
+    }
+
+    @PutMapping(value = {"/api/employees/{employee}", "/api/employees/{employee}/"})
+    public ResponseEntity<Employee> update(@RequestBody Employee employee) {
+
+        if (employee.getUserId() == null) {
+
+            log.warn("Trying to update a employee without id");
+            return ResponseEntity.badRequest().build();
+
+        }
+
+        if (!employeeRepository.existsById(employee.getUserId())) {
+
+            log.warn("Trying to update a non existent employee");
+            return ResponseEntity.notFound().build();
+
+        }
+
+        Employee result = employeeRepository.save(employee);
+        return ResponseEntity.ok(result);
+
+    }
+
+    @DeleteMapping(value = {"/api/employees/{id}", "/api/employees/{id}/"})
+    public ResponseEntity<Employee> deleteById(@PathVariable Long id) {
+
+        if (!employeeRepository.existsById(id)) {
+
+            log.warn("Trying to delete a non existent employee");
+            return ResponseEntity.notFound().build();
+
+        }
+
+        employeeRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @DeleteMapping(value = {"/api/employees", "/api/employees/"})
+    public ResponseEntity<Employee> deleteAll() {
+
+        log.info("Deleting all employees...");
+        employeeRepository.deleteAll();
+        return ResponseEntity.noContent().build();
+
+    }
+
+}
