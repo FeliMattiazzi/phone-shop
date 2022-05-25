abab37b9ea762757bc74dd709d6bda98a815a4a8
Germán Zittermann <germanzittermann@gmail.com>
2022-05-25 04:13:03 -0300

    Lombok implemented

--- src/main/java/com/phoneshop/entities/Depository.java
+++ src/main/java/com/phoneshop/entities/Depository.java
@@ -2,9 +2,12 @@ package com.phoneshop.entities;
 
 import com.phoneshop.models.Place;
 import com.phoneshop.models.Supply;
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.Set;
 
+@Data
 @Entity
 @Table(name = "DEPOSITORIES")
 public class Depository extends Place {
